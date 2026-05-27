330878ce481569a1fff81de93c02083094616ab2
eericsm <eeric.segawa@gmail.com>
2026-05-27 13:48:50 -0300

    base api

---
+++ scrappingford/apis/api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routers import cars
+
+app = FastAPI()
+
+app.include_router(cars.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+# /{name of brand} -> returns all data from that brand
+# /{name of brand}/{name of car} -> returns data from that car
+# /{name of brand}/{year} -> returns only cars from that year
